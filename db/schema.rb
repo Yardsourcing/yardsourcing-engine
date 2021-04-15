673255e384ec67521d25bfdbc263c9223200bf05
Jenny Branham <branhamjenny@gmail.com>
2021-04-14 19:32:20 -0500

    Create migration to hange zipcode to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_212413) do
+ActiveRecord::Schema.define(version: 2021_04_15_003048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2021_04_13_212413) do
     t.string "street_address"
     t.string "city"
     t.string "state"
-    t.integer "zipcode"
+    t.string "zipcode"
     t.decimal "price"
     t.string "description"
     t.string "availability"
