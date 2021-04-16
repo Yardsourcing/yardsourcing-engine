70bdd81e58b146d04153d7f399eb0f8594196f48
Jenny Branham <branhamjenny@gmail.com>
2021-04-16 09:11:30 -0500

    Add validation for unique index

--- spec/models/yard_purpose_spec.rb
+++ spec/models/yard_purpose_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe YardPurpose, type: :model do
     it { should belong_to :purpose }
   end
 
+  describe 'validations' do
+    it 'validates uniqueness of purpose for a yard' do
+      create(:yard_purpose)
+      should validate_uniqueness_of(:purpose_id).scoped_to(:yard_id)
+    end
+  end
+
   describe "class methods" do
     describe "::find_unseleted_purposes" do
       it "find all purposes that have been unselected by the user" do
