226be3e1a86745e53b47b1e7cc14e0f521b742d3
Dominic Padula <domopadula2192@gmail.com>
2021-04-17 14:15:59 -0600

    fix spacing

--- app/controllers/api/v1/yards/bookings_controller.rb
+++ app/controllers/api/v1/yards/bookings_controller.rb
@@ -2,16 +2,16 @@ class Api::V1::Yards::BookingsController < ApplicationController
   before_action :validate_params, only: :index
 
   def index
-  yard = Yard.find(params[:id])
-  bookings = yard.bookings
-  if bookings.empty?
-    render json: NullSerializer.new
-  else
-    render json: BookingSerializer.new(bookings)
+    yard = Yard.find(params[:id])
+    bookings = yard.bookings
+    if bookings.empty?
+      render json: NullSerializer.new
+    else
+      render json: BookingSerializer.new(bookings)
+    end
   end
-end
 
-private
+  private
   def validate_params
     if params[:id].to_i == 0
       render json: {error: "String not accepted as id"}, status: :bad_request
