2b214e7eb8b22cfaae7f655239a4121f6244faa9
abreaux26 <angelbreaux26@gmail.com>
2021-04-17 16:02:35 -0500

    Refactor to include show all bookings by renter
    Co-authored-by: Dominic Padula <70593322+domo2192@users.noreply.github.com>

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,19 +1,15 @@
-module Api
-  module V1
-    class BookingsController < ApplicationController
-      before_action :validate_params, only: :show
+class Api::V1::BookingsController < ApplicationController
+  before_action :validate_params, only: :show
 
-      def show
-        booking = Booking.find(params[:id])
-        render json: BookingSerializer.new(booking)
-      end
+  def show
+    booking = Booking.find(params[:id])
+    render json: BookingSerializer.new(booking)
+  end
 
-      private
-      def validate_params
-        if params[:id].to_i == 0
-          render json: {error: "String not accepted as id"}, status: :bad_request
-        end
-      end
+  private
+  def validate_params
+    if params[:id].to_i == 0
+      render json: {error: "String not accepted as id"}, status: :bad_request
     end
   end
 end
