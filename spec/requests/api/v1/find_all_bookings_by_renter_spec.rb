114d69b6e2bfa505df648873222a60549272f2fd
abreaux26 <angelbreaux26@gmail.com>
2021-04-17 16:00:48 -0500

    Create spec for showing all bookings by renter
    Co-authored-by: Dominic Padula <70593322+domo2192@users.noreply.github.com>

---
+++ spec/requests/api/v1/find_all_bookings_by_renter_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+RSpec.describe 'All Bookings by Renter API' do
+  before :each do
+    create_list(:booking, 2, status: 'pending')
+    create_list(:booking, 2, status: 'rejected')
+    create_list(:booking, 2, status: 'approved')
+  end
+
+  describe 'happy path' do
+    it 'returns all bookings' do
+      get "/api/v1/renters/1/bookings"
+
+      expect(response).to be_successful
+
+      bookings = JSON.parse(response.body, symbolize_names:true)
+
+      expect(bookings).to be_a(Hash)
+      expect(bookings[:data]).to be_an(Array)
+      expect(bookings[:data].count).to eq(6)
+      bookings[:data].each do |booking|
+        expect(booking[:type]).to eq('booking')
+        expect(booking[:attributes].count).to eq(8)
+        expect(booking[:attributes]).to have_key(:status)
+        expect(booking[:attributes]).to have_key(:booking_name)
+        expect(booking[:attributes]).to have_key(:date)
+        expect(booking[:attributes]).to have_key(:description)
+        expect(booking[:attributes]).to have_key(:duration)
+        expect(booking[:attributes]).to have_key(:time)
+        expect(booking[:attributes]).to have_key(:renter_id)
+        expect(booking[:attributes]).to have_key(:yard_id)
+        expect(booking[:attributes][:renter_id]).to be_a(Integer)
+        expect(booking[:attributes][:yard_id]).to be_a(Integer)
+        expect(booking[:attributes][:duration]).to be_a(Integer)
+        expect(booking[:attributes][:description]).to be_a(String)
+        expect(booking[:attributes][:status]).to be_a(String)
+        expect(booking[:attributes][:booking_name]).to be_a(String)
+        expect(booking[:attributes][:time]).to be_a(String)
+      end
+    end
+  end
+
+  describe 'sad path' do
+    it 'returns an empty array if no bookings exist' do
+      booking = create(:booking, renter_id: 2)
+      create_list(:booking, 7, renter_id: 2)
+      create_list(:booking, 2, renter_id: 2)
+
+      get "/api/v1/renters/3/bookings"
+
+      expect(response).to be_successful
+      bookings = JSON.parse(response.body, symbolize_names:true)
+      expect(bookings).to be_a(Hash)
+      expect(bookings[:data]).to be_a(Hash)
+      expect(bookings[:data].empty?).to eq(true)
+    end
+
+    it 'returns an error if string for renter_id' do
+      booking = create(:booking, renter_id: 2)
+      create_list(:booking, 7, renter_id: 2)
+      create_list(:booking, 2, renter_id: 2)
+
+      get "/api/v1/renters/three/bookings"
+
+      bookings = JSON.parse(response.body, symbolize_names:true)
+      expect(bookings).to be_a(Hash)
+      expect(bookings[:error]).to eq("String not accepted as id")
+    end
+  end
+end
