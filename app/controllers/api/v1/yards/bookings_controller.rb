9ed291ed9aa8a59191c6607c46ecf4c75a543073
Dominic Padula <domopadula2192@gmail.com>
2021-04-17 14:14:57 -0600

    typo

--- app/controllers/api/v1/yards/bookings_controller.rb
+++ app/controllers/api/v1/yards/bookings_controller.rb
@@ -1,26 +1,20 @@
-module Api
-  module V1
-    module Yards
-      class BookingsController < ApplicationController
-        before_action :validate_params, only: :index
+class Api::V1::Yards::BookingsController < ApplicationController
+  before_action :validate_params, only: :index
 
-        def index
-          yard = Yard.find(params[:id])
-          bookings = yard.bookings
-          if bookings.empty?
-            render json: NullSerializer.new
-          else
-            render json: BookingSerializer.new(bookings)
-          end
-        end
+  def index
+  yard = Yard.find(params[:id])
+  bookings = yard.bookings
+  if bookings.empty?
+    render json: NullSerializer.new
+  else
+    render json: BookingSerializer.new(bookings)
+  end
+end
 
-        private
-        def validate_params
-          if params[:id].to_i == 0
-            render json: {error: "String not accepted as id"}, status: :bad_request
-          end
-        end
-      end
+private
+  def validate_params
+    if params[:id].to_i == 0
+      render json: {error: "String not accepted as id"}, status: :bad_request
     end
   end
 end
