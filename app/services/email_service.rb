0a940372446f9201170251ef64191ce70059347f
DougWelchons <doug.welchons@gmail.com>
2021-04-18 16:16:22 -0600

    Add funtioning send email method

---
+++ app/services/email_service.rb
@@ -0,0 +1,17 @@
+class EmailService
+  def self.send_email(to, from, subject, content)
+    response = connection.post('/api/v1/mail') do |req|
+      req.headers["CONTENT_TYPE"] = "application/json"
+      req.params = {to: to, from: from, subject: subject, content: content}
+    end
+    return true if response.status == 202
+    false
+  end
+
+  def self.connection
+    Faraday.new(url: "https://peaceful-bastion-57477.herokuapp.com")
+  end
+
+  # "http://localhost:9292"
+  # "https://peaceful-bastion-57477.herokuapp.com"
+end
--- spec/services/email_service_spec.rb
+++ spec/services/email_service_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe EmailService do
         subject = "new booking created!"
         content = "a new booking has been created, click here to approve, or click here to reject"
         method_call = EmailService.send_email(to, from, subject, content)
-        expect(method_call.status).to eq(202)
+        expect(method_call).to eq(true)
       end
     end
   end
