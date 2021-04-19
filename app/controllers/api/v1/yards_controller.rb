824df21413437f09571c122a9de7778cda49ecd0
Gvieve <gnuebel13@gmail.com>
2021-04-19 11:51:51 -0600

    Refactor errors to render_error method in yards_controller

--- app/controllers/api/v1/yards_controller.rb
+++ app/controllers/api/v1/yards_controller.rb
@@ -59,13 +59,15 @@ class Api::V1::YardsController < ApplicationController
 
   def validate_yard_params
     if params[:id].to_i == 0
-      render json: {error: "String not accepted as id"}, status: :bad_request
+      error = "String not accepted as id"
+      render_error(error)
     end
   end
 
   def validate_host_params
     if params[:host_id].to_i == 0
-      render json: {error: "String not accepted as id"}, status: :bad_request
+      error = "String not accepted as id"
+      render_error(error)
     end
   end
 
