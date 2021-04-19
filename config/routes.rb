584b0f0c8aee59c053a5be20ca20d048a0d67225
Gvieve <gnuebel13@gmail.com>
2021-04-19 13:43:12 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,22 +8,18 @@ Rails.application.routes.draw do
         get '/yard_search', to: 'search#search'
         get '/:id/bookings', to: 'bookings#index'
       end
-      resources :yards, except: [:index] do
-        # resources :bookings, only: [:index]
-      end
+
+      resources :yards, except: [:index]
 
       resources :bookings, only: [:show, :create, :update, :destroy]
 
       resources :hosts, only: [] do
-        resources :yards, only: [:index]
+        resources :yards, only: :index
+        resources :bookings, controller: "hosts/bookings", only: :index
       end
 
-      namespace :renters do
-        get '/:renter_id/bookings', to: 'bookings#index'
-      end
-
-      namespace :hosts do
-        get '/:host_id/bookings', to: 'bookings#index'
+      resources :renters , only: [] do
+        resources :bookings, controller: "renters/bookings", only: :index
       end
     end
   end
