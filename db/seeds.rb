557fbc53f5b632ad695266ea1857b9f0c8904447
abreaux26 <angelbreaux26@gmail.com>
2021-04-20 13:43:19 -0500

    Add one more pending for renter id 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -152,6 +152,14 @@ yard5.bookings.create(renter_id: 3,
                       time: Time.new(2021,04,25,20).strftime("%H:%M"),
                       duration: 8,
                       description: 'Trying to throw a rager with my friends.')
+yard5.bookings.create(renter_id: 1,
+                      renter_email: "email@domain.com",
+                      status: :pending,
+                      booking_name: 'Country Throw Down Part 2',
+                      date: Date.new(2021,04,25),
+                      time: Time.new(2021,04,25,20).strftime("%H:%M"),
+                      duration: 5,
+                      description: 'Trying to throw a rager with my friends again.')
 
 ActiveRecord::Base.connection.tables.each do |t|
   ActiveRecord::Base.connection.reset_pk_sequence!(t)
